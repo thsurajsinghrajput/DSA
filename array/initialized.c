0d3407470936961c103699de275ceb0ad32eedf1
thsurajsinghrajput <thsurajsinghrajput@gmail.com>
2023-12-06 18:03:43 +0530

    DSA update

--- array/initialized.c
+++ array/initialized.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(){
-  int size;
+  int size,i;
   printf("\nEnter the size of the array: ");
   scanf(" %d", &size);
   int arr[size];
   printf("\nenter array elements\n");
-  for(int i=0; i<size; i++){
+  for( i=0; i<size; i++){
     scanf("%d" , &arr[i]);
   }
   printf("\nyour array is: ");
-  for(int i=0; i<size; i++){
+  for( i=0; i<size; i++){
     printf("%d\t" , arr[i]);
   }
   return 0;
